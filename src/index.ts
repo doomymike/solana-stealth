f3d094a5a70c8162f2c879669cd7170b7d4a9ffa
Michael Oren <michael@alexoren.com>
2023-03-14 12:54:17 -0500

    revert testing change

--- src/index.ts
+++ src/index.ts
@@ -578,7 +578,7 @@ export async function sendFromStealth(
 
   let a: BlockheightBasedTransactionConfirmationStrategy = {blockhash: bhash.blockhash, signature: b, lastValidBlockHeight: bhash.lastValidBlockHeight};
 
-  const txid = sendAndConfirmRawTransaction(connection, tx.serialize());
+  const txid = sendAndConfirmRawTransaction(connection, tx.serialize(), a);
   return txid;
 }
 
